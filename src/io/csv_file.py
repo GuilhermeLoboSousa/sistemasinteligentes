7f3c1811c26312031180db6f88bb5642d93647e9
GuilhermeLoboSousa <120011013+GuilhermeLoboSousa@users.noreply.github.com>
2023-09-22 18:55:16 +0100

    read csv

---
+++ src/io/csv_file.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+from si.data.dataset import Dataset
+
+def read_csv (filename:str, sep:str=",", features:bool=None, label:bool=None ):
+    """
+    allow to read a csc file into a Dataset object
+    
+    Arguments
+    --------------
+    filename: the path tho the file
+    sep: separator used in the file (most comumn is , or ;) 
+    features: if the file has a header- default is false
+    label: if the file has a label y- default is false
+    """
+    data=pd.read_csv(filename,sep=sep)
+    if features and label is not None:
+        features=data.columns[:-1] # nome de todas as coluans menos a ultim
+        label=data.columns[-1] # nome da coluna label
+        X=data.iloc[:,:-1].to_numpy() #todas as linhas e todas as colunas menos a ultima
+        y=data.iloc[:,-1].to_numpy() #todas as linhas apenas da ultima coluna
+
+    elif features is not None and label is None:
+        features=data.columns
+        X=data.to_numpy()
+        y=None
+
+    elif features is None and label is not None:
+        features=None
+        label=data.columns[-1] #eu penso que o mais correto seria isto pq eu sei que a ultima vai ser a label y- perguntar ao prof 
+        X=data.iloc[:,:-1].to_numpy()
+        y=data.iloc[:,-1].to_numpy()
+
+    else:# sem features nem label
+        X = data.to_numpy() # é tudo considerado X
+        y = None #portanto nenhuma é y
+        features = None # reforçar a propria condição
+        label = None    
+
+    return Dataset(X,y,features=features, label=label) #vai buscar a classe já criada, o que tivemos aqui a fazer foi preparar para ler o ficheiro consoante certas condições explicitas anteriormente
