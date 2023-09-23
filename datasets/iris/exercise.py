99ba544b202d3a9187070da46165490e32bb2cdd
GuilhermeLoboSousa <120011013+GuilhermeLoboSousa@users.noreply.github.com>
2023-09-23 12:35:07 +0100

    alter

--- datasets/iris/exercise.py
+++ datasets/iris/exercise.py
@@ -1,12 +1,10 @@
 import numpy as np
+import pandas as pd
 from src.si.data.dataset import Dataset
 from src.io.csv_file import read_csv, write_csv 
 from src.io.data_file import *
 
-filename="iris.csv"
-import os
+filename = r"C:\Users\guilh\OneDrive\Documentos\GitHub\sistemasinteligentes\datasets\iris\iris.csv"
 
-current_directory = os.getcwd()
-print("Diretório Atual:", current_directory)
 dataset=read_csv(filename, sep=",")
 dataset
