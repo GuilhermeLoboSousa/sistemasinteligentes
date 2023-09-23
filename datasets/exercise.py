e95bd88b3a855e638322b33b16a06ecabf299679
GuilhermeLoboSousa <120011013+GuilhermeLoboSousa@users.noreply.github.com>
2023-09-23 12:29:55 +0100

    feito

--- datasets/exercise.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from src.si.data.dataset import Dataset
-from src.io.csv_file import read_csv, write_csv 
-from src.io.data_file import *
-
-filename="iris.csv"
-
-dataset=read_csv(filename, sep=",")
-dataset
