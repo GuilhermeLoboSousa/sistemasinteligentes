274bff563301807ef9eeef5fb3dcda7b3d8eb3f1
GuilhermeLoboSousa <120011013+GuilhermeLoboSousa@users.noreply.github.com>
2023-09-27 23:23:12 +0100

    acabei percentile

--- src/si/data/test.py
+++ src/si/data/test.py
@@ -1,17 +1,7 @@
 import numpy as np
 
-# Seus dados da coluna y (exemplo)
-y_data = np.array([[1, 0], [0, 1], [1, np.nan], [1, 0], [0, np.nan], [1, 0], [0, 1], [0, 1], [1, 0]])
+X=[8.33333333e+00, 4.11522634e-03, 1.81694583e+01, 8.65076540e-02]
+top_50=np.percentile(X,75)
+indices = np.where(X <= top_50)[0]
 
-novo=np.isnan(y_data).any(axis=1)
-indice=np.where(novo)
-indice
-y_data[~novo]
-y_data[novo]
-nan_columns = np.isnan(y_data).any(axis=0) 
-a=np.where(nan_columns)[0]
-for x in a:
-    print(a)
-    col = y_data[:, a]
-    np.isnan(col)
-    col[np.isnan(col)]
+print(top_50,indices)
--- src/si/feature_selection/percentile.py
+++ src/si/feature_selection/percentile.py
@@ -1,27 +1,95 @@
+import sys
+sys.path.append("C:\\Users\\guilh\\OneDrive\\Documentos\\GitHub\\sistemasinteligentes")
+
 from typing import Callable
 
 import numpy as np
 
-from si.data.dataset import Dataset
-from si.statistics.f_classification import f_classification
+from src.si.data.dataset import Dataset
+from src.si.statistics.f_classification import f_classification
 
-class percentile:
+class Percentile:
+    """
+    select a certain percentage of the features taking into account the F-score value.
+    this is first we see the f-score of each feature and sorted that.
+    after we choose a percentil that representes x % of this f-values sorted
+    so we keep the features that indices have the f-value <= to the percentile
+    
+    Parameters
+    -----------
+    score_func: taking the dataset and return a pair os array (F and p value)- allow analize the variance 
+    percentile: number that represents a percentage of the data 
+
+    estimated parameters(given by the score_func)
+    ---------------
+    F:
+    p:
+
+    """
     def __init__(self, score_func: Callable = f_classification, percentile:int=50 ) -> None:
         self.percentile = percentile
         self.score_func = score_func
         self.F = None
         self.p = None
 
+        if self.percentile > 100 or self.percentile < 0:
+            raise ValueError("the value of percentile must be between 0 and 100")
+
     def fit (self,dataset:Dataset) -> "percentile":
+        """
+        estimates the F and p values for each feature using the score_func
+
+        Parameters
+        -------
+        Dataset
+
+        Return---
+        itself
+        """        
         self.F, self.p = self.score_func(dataset)
         return self
-        pass
 
     def transform (self, dataset:Dataset) -> Dataset:
-        top_50=np.percentile(self.F,self.percentile)
-        features=np.array(dataset.features)[top_50]
-        return Dataset(X=dataset.X[:,top_50], y=dataset.y, features=features, label=dataset.label) 
+        """
+        select the percentage of the data (percentile) that we want taking into acount the F-value of each feature
+
+        Returns:
+        the select (filtered) dataset
+        """
+        print(self.F)        
+        top_50=np.percentile(self.F,self.percentile) #nao preciso de ordenar primeiro os valores, para calcular o percentile a ordem nao importa(ja testei isso)
+        indices = np.where(self.F <= top_50)[0] # indices vai dizer a posição dos valores de F que têm valor menor que o percentile, ou seja identifica os indices das colunas que devem ser escolhidas
+        features=np.array(dataset.features)[self.F <= top_50] #mesma logica mas para ir buscar o nome das mesmas
+        return Dataset(X=dataset.X[:,indices], y=dataset.y, features=features, label=dataset.label) 
     
     def fit_transform(self,dataset:Dataset) -> Dataset:
+        """
+        Runs fit and then transform the dataaset taking into acount a certain percentile of the fvalue score.
+
+        return
+        ------
+        dataset already filtered by a percentile value (where the key is the f-score) 
+        """        
         self.fit(dataset)
         return self.transform(dataset)
+    
+
+
+#testing
+if __name__ == '__main__':
+    from src.si.data.dataset import Dataset
+
+    dataset = Dataset(X=np.array([[0.2, 2, 0.06, 2.87],
+                                  [0.5, 1.5, 4, 3],
+                                  [0.3, 1.1, 1, 3.4]]),
+                      y=np.array([0, 1, 0]),
+                      features=["f1", "f2", "f3", "f4"],
+                      label="y")
+
+
+    percentiles = [50,25,75]
+    for percentile in percentiles:
+        selector = Percentile(percentile=percentile)
+        selector = selector.fit(dataset)
+        dataset_filtered = selector.transform(dataset)
+        print(f"Features for percentile {percentile}: {dataset_filtered.features}")
