bd48d003c765e7118eaa7b748859668baeddbc4e
GuilhermeLoboSousa <120011013+GuilhermeLoboSousa@users.noreply.github.com>
2023-10-17 21:55:19 +0100

    sigmoid

---
+++ src/si/statistics/sigmoid_function.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append("C:\\Users\\guilh\\OneDrive\\Documentos\\GitHub\\sistemasinteligentes")
+import numpy as np
+
+
+def sigmoid_function(X: np.ndarray) -> np.ndarray:
+    """
+    It returns the sigmoid function of the given input
+
+    Parameters
+    ----------
+    X: np.ndarray
+        The input of the sigmoid function
+
+    Returns
+    -------
+    sigmoid: np.ndarray
+        The sigmoid function of the given input
+    """
+    return 1 / (1 + np.exp(-X))
