5253e4d54478dcfd3e56675ebc0b14de3aede875
GuilhermeLoboSousa <120011013+GuilhermeLoboSousa@users.noreply.github.com>
2023-11-28 18:28:31 +0000

    alterações na accuracy

--- src/si/metrics/accuracy.py
+++ src/si/metrics/accuracy.py
@@ -20,7 +20,18 @@ def accuracy(y_true:np.ndarray,y_pred:np.ndarray) -> float:
     accuracy: float
         The accuracy of the model
     """
-    return np.sum(y_true==y_pred)/len(y_true) #o y_pred vai ser algo que vai de acordo ou nao ao que esta no dataset (y_true), logo ao fazer == verificamos onde se verifica true negative as positve
+    def correct_format(y):
+        if len(y[0]) == 1:
+            corrected_y = [np.round(y[i][0]) for i in range(len(y))]
+        else:
+            corrected_y = [np.argmax(y[i]) for i in range(len(y))]
+        return np.array(corrected_y)
+    if isinstance(y_true[0], list) or isinstance(y_true[0], np.ndarray):
+        y_true = correct_format(y_true)
+    if isinstance(y_pred[0], list) or isinstance(y_pred[0], np.ndarray):
+        y_pred = correct_format(y_pred)
+    return np.sum(y_pred == y_true) / len(y_true)
+     #o y_pred vai ser algo que vai de acordo ou nao ao que esta no dataset (y_true), logo ao fazer == verificamos onde se verifica true negative as positve
 #esse == dá um array de boleanos que podemos contar fazendo o np.sum
 # e depois dividimos por o len de y_tru ou de y_pred pois quer um ou outro vai ter todas as possibilidades quer seja TN,TP,FN,FP
 #indo por isso ao encontro da formula
